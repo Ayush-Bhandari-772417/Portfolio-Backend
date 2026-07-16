447ed596e31bc5b962a8ed65b07a6f68fd2ba95a
Ayush Bhandari <er.st.ayushbhandari@gmail.com>
2026-07-16 23:33:28 +0545

    middleware removed from settings

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'seo.middleware.AICrawlerTrackingMiddleware',
 ]
 
 ROOT_URLCONF = "config.urls"
